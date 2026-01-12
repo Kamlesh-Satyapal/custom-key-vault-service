0cc07b7962cd39d1319a8fcf3e6903ccfec09ab5
Kamlesh Satyapal <satyapal.kamlesh@gmail.com>
2026-01-12 13:11:56 +0530

    adding secret crud service

---
+++ Secret-CRUD-Service/src/main/java/com/secret/crud/secret_crud_service/SecretCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.secret.crud.secret_crud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretCrudServiceApplication.class, args);
+	}
+
+}
---
+++ Secret-CRUD-Service/src/test/java/com/secret/crud/secret_crud_service/SecretCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secret.crud.secret_crud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
